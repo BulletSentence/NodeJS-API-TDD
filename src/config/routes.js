b95302a77ca2de91ff063010fc72952ddf42da54
Leonardo Almeida de Araújo <leonardo.a@acad.ifma.edu.br>
2023-04-03 21:39:19 -0300

    Added /accounts
    - Test for creating a account

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,4 +2,7 @@ module.exports = (app) => {
   app.route("/users")
   .get(app.routes.user.findAll)
   .post(app.routes.user.create);
+
+  app.route("/accounts")
+  .post(app.routes.accounts.create);
 };
---
+++ src/migrations/20230403223218_create_table_accounts.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => {
+  return knex.schema.createTable("accounts", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.integer("user_id").references("id").inTable("users").notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => {
+    return knex.schema.dropTable("accounts");
+};
